3934a24666e891f09e92762275da8a008e808c4e
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-03 19:43:38 +0100

    delete else é indefinido

--- Modulo 04 - Fundamentos em Front-End/JavaScript/aula6.1_Desafio_Guiado_Funcao_Palindromo/script.js
+++ Modulo 04 - Fundamentos em Front-End/JavaScript/aula6.1_Desafio_Guiado_Funcao_Palindromo/script.js
@@ -10,9 +10,7 @@ function palindromo(s) {
   //implementar
   if (s.length <= 1) {
     console.log(s + " é palíndromo");
-  } else {
-    console.log(s + " é indefinido");
-  }
+  } 
 
   for (var i = 0, j = s.length - 1; i < j; i++, j--) {
     if (s.charAt(i) != s.charAt(j)) {
