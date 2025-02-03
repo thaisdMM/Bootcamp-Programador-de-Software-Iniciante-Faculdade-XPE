7536eee744ef45f5b332d4082f735bbef7a17abb
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-03 19:10:05 +0100

    Update script.js

---
+++ Modulo 04 - Fundamentos em Front-End/JavaScript/aula6.1_Desafio_Guiado_Funcao_Palindromo/script.js
@@ -0,0 +1,11 @@
+palindromo("aba"); // é palindromo
+palindromo("radar"); // é palindromo
+palindromo("xyzzyx"); // é palindromo
+palindromo("bola"); // não é palindromo
+palindromo("abcddbc"); // não é palindromo
+palindromo(""); // é palindromo
+palindromo("x"); // é palindromo
+
+function palindromo(s) {
+  //implementar
+}
