87abfa58b53fb3b6976299aa4cde4e7213842052
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-03 19:41:37 +0100

    implementing for to find palindromo

--- Modulo 04 - Fundamentos em Front-End/JavaScript/aula6.1_Desafio_Guiado_Funcao_Palindromo/script.js
+++ Modulo 04 - Fundamentos em Front-End/JavaScript/aula6.1_Desafio_Guiado_Funcao_Palindromo/script.js
@@ -10,18 +10,15 @@ function palindromo(s) {
   //implementar
   if (s.length <= 1) {
     console.log(s + " é palíndromo");
-  }  else {
+  } else {
     console.log(s + " é indefinido");
   }
 
-  for (var i= 0, j = s.length -1; i < j; i++, j--) {
-   if(s.charAt(i) == s.charAt(j)){
-
-   }else{
-      console.log(s + 'não é palíndromo')
+  for (var i = 0, j = s.length - 1; i < j; i++, j--) {
+    if (s.charAt(i) != s.charAt(j)) {
+      console.log(s + " não é palíndromo");
       return;
-   }
-
+    }
   }
-
+  console.log(s + " é palíndromo");
 }
