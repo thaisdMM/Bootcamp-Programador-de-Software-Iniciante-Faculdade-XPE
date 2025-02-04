9e5ddb8d2386c5564cf0b2894277b4ea182589ce
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-04 10:22:02 +0100

    add function calcula to script

---
+++ Modulo 04 - Fundamentos em Front-End/Interacao com o DOM/5.1_intrecao_basica_com_DOM/base/soma.js
@@ -0,0 +1,8 @@
+var n1 = document.getElementById('n1');
+var n2 = document.getElementById('n2');
+
+function calcula () {
+   var resultado = n1.valueAsNumber + n2.valueAsNumber;
+   console.log(resultado);
+}
+
