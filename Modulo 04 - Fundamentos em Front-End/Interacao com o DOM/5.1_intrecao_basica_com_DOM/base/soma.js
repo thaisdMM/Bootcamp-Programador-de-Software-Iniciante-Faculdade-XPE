6877538ca0ed46b268096b2feeaded5342924d73
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-04 10:31:54 +0100

    add var r and r.textcontent
    result printed on html page

--- Modulo 04 - Fundamentos em Front-End/Interacao com o DOM/5.1_intrecao_basica_com_DOM/base/soma.js
+++ Modulo 04 - Fundamentos em Front-End/Interacao com o DOM/5.1_intrecao_basica_com_DOM/base/soma.js
@@ -1,8 +1,8 @@
-var n1 = document.getElementById('n1');
-var n2 = document.getElementById('n2');
+var n1 = document.getElementById("n1");
+var n2 = document.getElementById("n2");
+var r = document.getElementById("r");
 
-function calcula () {
-   var resultado = n1.valueAsNumber + n2.valueAsNumber;
-   console.log(resultado);
+function calcula() {
+  var resultado = n1.valueAsNumber + n2.valueAsNumber;
+  r.textContent = resultado;
 }
-
