6db8ece442ec39ea509a5f11973c6009a4ca8de0
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-04 15:56:38 +0100

    changing to add the line before the addButton

--- Modulo 04 - Fundamentos em Front-End/Interacao com o DOM/5.1_intrecao_basica_com_DOM/aula3_Criando_Elementos/script.js
+++ Modulo 04 - Fundamentos em Front-End/Interacao com o DOM/5.1_intrecao_basica_com_DOM/aula3_Criando_Elementos/script.js
@@ -1,23 +1,21 @@
-
 var fslist = document.querySelectorAll(".multiple-field");
 
-for (var i = 0; i < fslist.length; i++){
-   initMultipleFieldSet(fslist[i]);
+for (var i = 0; i < fslist.length; i++) {
+  initMultipleFieldSet(fslist[i]);
 }
 
 function initMultipleFieldSet(fs) {
-   var addButton = document.createElement("button");
-   addButton.textContent = "Adicionar";
-   addButton.type = "button";
+  var addButton = document.createElement("button");
+  addButton.textContent = "Adicionar";
+  addButton.type = "button";
 
-   fs.appendChild(addButton);
+  fs.appendChild(addButton);
 
-   addButton.addEventListener("click", function() {
-      var div = document.createElement("div");
-      var newInput = document.createElement("input");
-      div.appendChild(newInput);
+  addButton.addEventListener("click", function () {
+    var div = document.createElement("div");
+    var newInput = document.createElement("input");
+    div.appendChild(newInput);
 
-      fs.appendChild(div);
-
-   });
+    fs.insertBefore(div, addButton);
+  });
 }
