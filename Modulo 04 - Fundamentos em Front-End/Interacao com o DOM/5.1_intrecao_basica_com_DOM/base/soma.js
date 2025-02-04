ad54615c6f1a33ff9bed1233684a4ab935237fbd
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-04 10:35:59 +0100

    NaN case

--- Modulo 04 - Fundamentos em Front-End/Interacao com o DOM/5.1_intrecao_basica_com_DOM/base/soma.js
+++ Modulo 04 - Fundamentos em Front-End/Interacao com o DOM/5.1_intrecao_basica_com_DOM/base/soma.js
@@ -4,5 +4,9 @@ var r = document.getElementById("r");
 
 function calcula() {
   var resultado = n1.valueAsNumber + n2.valueAsNumber;
+
+  if (isNaN(resultado)) {
+   r.textContent = '';
+  }else
   r.textContent = resultado;
 }
