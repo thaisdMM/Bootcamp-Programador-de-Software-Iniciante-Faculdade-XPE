e50964b6cf2bbdc4ec06472e5e120aa8121978b3
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-05 09:16:42 +0100

    changing caixa.style

--- Modulo 04 - Fundamentos em Front-End/Interacao com o DOM/5.1_intrecao_basica_com_DOM/aula4_Alterando_Estilos/script.js
+++ Modulo 04 - Fundamentos em Front-End/Interacao com o DOM/5.1_intrecao_basica_com_DOM/aula4_Alterando_Estilos/script.js
@@ -1,5 +1,6 @@
 var caixa = document.getElementById("caixa");
 
 function setPos(x, y) {
-  caixa.style = "top: " + y + "px; left: " + x + "px";
+  caixa.style.top = y + "px";
+  caixa.style.left = x + "px";
 }
