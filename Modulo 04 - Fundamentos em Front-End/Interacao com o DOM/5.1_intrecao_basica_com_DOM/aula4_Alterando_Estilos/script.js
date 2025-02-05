36bb0f5e28d652afdfbcd22185ef82b4c7241645
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-05 10:00:09 +0100

    mousemove only in the box(caixa) element

--- Modulo 04 - Fundamentos em Front-End/Interacao com o DOM/5.1_intrecao_basica_com_DOM/aula4_Alterando_Estilos/script.js
+++ Modulo 04 - Fundamentos em Front-End/Interacao com o DOM/5.1_intrecao_basica_com_DOM/aula4_Alterando_Estilos/script.js
@@ -6,13 +6,20 @@ function setPos(x, y) {
 }
 
 caixa.addEventListener("mousedown", iniciaArraste);
+document.addEventListener("mouseup", terminaArraste);
 
 function iniciaArraste(evt) {
   caixa.classList.add("arrastando");
+  caixa.addEventListener("mousemove", arrasta);
 }
-
-document.addEventListener("mouseup", terminaArraste);
 
 function terminaArraste(evt) {
   caixa.classList.remove = "arrastando";
+  caixa.removeEventListener("mousemove", arrasta);
+}
+
+function arrasta(evt) {
+  var x = evt.clientX;
+  var y = evt.clientY;
+  setPos(x, y);
 }
