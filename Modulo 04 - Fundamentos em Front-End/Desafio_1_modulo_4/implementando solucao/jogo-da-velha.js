35482d2ce0edcb0a82a45232f883132b30fbec4f
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-06 19:17:10 +0100

    implementing td

--- Modulo 04 - Fundamentos em Front-End/Desafio_1_modulo_4/implementando solucao/jogo-da-velha.js
+++ Modulo 04 - Fundamentos em Front-End/Desafio_1_modulo_4/implementando solucao/jogo-da-velha.js
@@ -1,3 +1,9 @@
 function fazJogada(l, c) {
+  var tbody = document.getElementById("tabuleiro");
+  var tr = tbody.children[l];
+  var td = tr.children[c];
+
+  td.textContent = "0";
+
   console.log("clicou na celula " + l + " " + c);
 }
