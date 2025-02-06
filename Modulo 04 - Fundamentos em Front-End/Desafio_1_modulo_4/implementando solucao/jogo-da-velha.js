917fde309c02c7f801cd6d8378274f993520f511
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-06 19:23:56 +0100

    trying implementing player 0 or X

--- Modulo 04 - Fundamentos em Front-End/Desafio_1_modulo_4/implementando solucao/jogo-da-velha.js
+++ Modulo 04 - Fundamentos em Front-End/Desafio_1_modulo_4/implementando solucao/jogo-da-velha.js
@@ -1,9 +1,17 @@
 function fazJogada(l, c) {
+  var proximoJogador = "O";
+
   var tbody = document.getElementById("tabuleiro");
   var tr = tbody.children[l];
   var td = tr.children[c];
 
-  td.textContent = "0";
+  td.textContent = proximoJogador;
+
+  if (proximoJogador == "O") {
+    proximoJogador == "X";
+  } else {
+    proximoJogador == "O";
+  }
 
   console.log("clicou na celula " + l + " " + c);
 }
