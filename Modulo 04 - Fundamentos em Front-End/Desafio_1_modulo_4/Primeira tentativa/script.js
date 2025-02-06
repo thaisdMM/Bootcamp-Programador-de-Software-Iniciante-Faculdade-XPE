6ae196d4c28f908a558070b16ad2617d5d29e244
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-06 17:31:45 +0100

    wrong trying

--- Modulo 04 - Fundamentos em Front-End/Desafio_1_modulo_4/Primeira tentativa/script.js
+++ Modulo 04 - Fundamentos em Front-End/Desafio_1_modulo_4/Primeira tentativa/script.js
@@ -1,1 +1,9 @@
 var ncels = document.querySelectorAll("td");
+
+function iniciatd(jv) {
+  var addButton = document.createElement("button");
+  addButton.click = "escolherJogada";
+  addButton.type = "button";
+}
+
+jv.appendChild(addButton);
