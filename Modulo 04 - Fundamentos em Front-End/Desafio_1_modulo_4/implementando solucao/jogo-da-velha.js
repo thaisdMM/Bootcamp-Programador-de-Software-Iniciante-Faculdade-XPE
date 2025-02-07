3a0838e1b9770bf76a5fd9361396523578e77fbe
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-07 12:38:24 +0100

    Update jogo-da-velha.js

--- Modulo 04 - Fundamentos em Front-End/Desafio_1_modulo_4/implementando solucao/jogo-da-velha.js
+++ Modulo 04 - Fundamentos em Front-End/Desafio_1_modulo_4/implementando solucao/jogo-da-velha.js
@@ -6,13 +6,11 @@ function fazJogada(l, c) {
   var td = tr.children[c];
 
   if (td.textContent == "") {
-    td.textContent = proximoJogador; //Melhor momento para verificar um vencedor é após terminar de marcar a jogada
+    td.textContent = proximoJogador;
 
     if (jogadorVenceu(proximoJogador)) {
-      //
       console.log("jogador venceu: " + proximoJogador);
     } else {
-      //se nao venceu continua a jogada
       if (proximoJogador == "O") {
         proximoJogador = "X";
       } else {
@@ -27,21 +25,18 @@ function jogadorVenceu(jog) {
     if (cel(i, 0) == jog && cel(i, 1) == jog && cel(i, 2) == jog) {
       return true;
     }
+    if (cel(0, i) == jog && cel(1, i) == jog && cel(2, i) == jog) {
+      return true;
+    }
   }
-  for (var j = 0; j < 3; j++) {
-    if (cel(0, j) == jog && cel(1, j) == jog && cel(2, j) == jog) {
-      return true;
-    }
-    if (cel(0, 0) == jog && cel(1, 1) == jog && cel(2, 2) == jog) {
-      return true;
-    }
-    if (cel(0, 2) == jog && cel(1, j) == jog && cel(2, 0) == jog) {
-      return true;
-    }
+  if (cel(0, 0) == jog && cel(1, 1) == jog && cel(2, 2) == jog) {
+    return true;
+  }
+  if (cel(0, 2) == jog && cel(1, 1) == jog && cel(2, 0) == jog) {
+    return true;
   }
   return false;
 }
-//função auxiliar para reduzir o tamanho do codigo jogadorVenceu
 function cel(l, c) {
   return tbody.children[l].children[c].textContent;
 }
