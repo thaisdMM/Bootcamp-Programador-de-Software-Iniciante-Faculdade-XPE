d27d4080784660693c9f94ae616d33dad7190ccf
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-08 15:39:37 +0100

    Update objects.js

--- Modulo 04 - Fundamentos em Front-End/JavaScript Object-Oriented Programming/instantiating_objcts/objects.js
+++ Modulo 04 - Fundamentos em Front-End/JavaScript Object-Oriented Programming/instantiating_objcts/objects.js
@@ -5,8 +5,31 @@
 function Retangulo(altura, largura) {
    this.altura = altura;
    this.largura = largura;
-   this.area = function() {
+   this.area = calculaArea;
+
+}
+//referenciando o this, mas nao está dentro do consultor Retangulo, mas refencia o objeto retangulo, mesmo a funççao estando do lado de fora
+function calculaArea() {
       return this.altura * this.largura;
    }
 
-}
+   /* teste no console
+   
+   Live reload enabled.
+var r1 = new Retangulo(4,7)
+undefined
+r1.area()
+28
+var fn = r1.area
+undefined
+fn()
+NaN
+calculaArea();
+NaN
+r1.area()
+28
+var fn2 = calculaArea.bind(r1)
+undefined
+fn2()
+28
+   */
