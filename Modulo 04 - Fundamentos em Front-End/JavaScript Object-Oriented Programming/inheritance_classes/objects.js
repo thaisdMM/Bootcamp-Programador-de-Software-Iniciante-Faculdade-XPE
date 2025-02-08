1c289c6f2bbfbd3c567ff4f91eac00e8cfcbe2ca
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-08 18:10:30 +0100

    add subclass
    - constructed a subclass that inherits "members" from the superclass

--- Modulo 04 - Fundamentos em Front-End/JavaScript Object-Oriented Programming/inheritance_classes/objects.js
+++ Modulo 04 - Fundamentos em Front-End/JavaScript Object-Oriented Programming/inheritance_classes/objects.js
@@ -9,5 +9,15 @@ class Retangulo {
   }
 }
 
+//extends é a herança e cria uma subclasse de retangulo
+//no construtor da subclasse tem que chamar o construtor da suplerclasse(super)
+class Quadrado extends Retangulo {
+  constructor(dimensao) {
+    super(dimensao, dimensao);
+  }
+}
+
 var r1 = new Retangulo(3, 4);
 var r2 = new Retangulo(3, 8);
+
+var r3 = new Quadrado(3);
