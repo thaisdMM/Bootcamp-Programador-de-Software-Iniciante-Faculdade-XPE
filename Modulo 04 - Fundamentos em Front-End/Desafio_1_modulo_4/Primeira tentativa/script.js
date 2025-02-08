67f312caf8ce050f848acde6bb4607711534d02d
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-08 11:42:00 +0100

    delete arquive with no correct code

--- Modulo 04 - Fundamentos em Front-End/Desafio_1_modulo_4/Primeira tentativa/script.js
+++
@@ -1,9 +0,0 @@
-var ncels = document.querySelectorAll("td");
-
-function iniciatd(jv) {
-  var addButton = document.createElement("button");
-  addButton.click = "escolherJogada";
-  addButton.type = "button";
-}
-
-jv.appendChild(addButton);
