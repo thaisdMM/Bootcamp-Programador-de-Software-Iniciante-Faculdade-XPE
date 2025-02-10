8b66969cbe393ee66ee83d4f0d60b6a084f30eea
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-10 12:03:31 +0100

    destructuring

--- Modulo 04 - Fundamentos em Front-End/modern_JavaScript/Let_Const_Destructuring_Spread_TempleteString/script.js
+++ Modulo 04 - Fundamentos em Front-End/modern_JavaScript/Let_Const_Destructuring_Spread_TempleteString/script.js
@@ -1,10 +1,10 @@
-// Declarar variáveis com let e const
+//1. Declarar variáveis com let e const
 
-let a = 1; // let pode ser alterada depois
+/* let a = 1; // let pode ser alterada depois
 const b = 1; // const não pode ser alterada depois
 
 a = 2;
-/*b = 2; erro de tipo, pois nao pode ser modificada depois*/
+//b = 2; erro de tipo, pois nao pode ser modificada depois
 
 function teste() {
   for (var i = 0; i < 5; i++) {
@@ -18,4 +18,15 @@ function teste() {
   }
   console.log("i: " + i);
 }
-teste();
+teste(); */
+
+//2. Usar atribuição via desestruturação
+
+let primos = [2, 3, 5, 7, 11, 13];
+
+let curso = {
+  nome: "Bootcamp Front End",
+  modulos: 4,
+  presencial: false,
+  turma: 1,
+};
