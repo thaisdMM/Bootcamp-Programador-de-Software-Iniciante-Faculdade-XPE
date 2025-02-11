4b4d33af44346f0a3146097a21fe2dbae52751dd
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-11 18:41:21 +0100

    template literals

--- Modulo 04 - Fundamentos em Front-End/modern_JavaScript/Let_Const_Destructuring_Spread_TempleteString/script.js
+++ Modulo 04 - Fundamentos em Front-End/modern_JavaScript/Let_Const_Destructuring_Spread_TempleteString/script.js
@@ -45,20 +45,49 @@ function imprime({nome}) {
    console.log(nome);
 }
 
-*/
+
 
 //3. Spread Operator
 
 let primos = [2, 3, 5, 7, 11, 13];
 let curso = {
-   nome: "Bootcamp Front End",
-   modulos: 4,
-   presencial: false,
-   turma: 1,
+  nome: "Bootcamp Front End",
+  modulos: 4,
+  presencial: false,
+  turma: 1,
 };
 
 // adicinou 17 no final de primos
-let primos2 = [... primos, 17];
+let primos2 = [...primos, 17];
 
 // adicionou 1 + primos + 17
 let primos3 = [1, ...primos, 17];
+
+//SPREAD para o objeto
+
+//spread para adcionar campos para mais
+let curso2 = {
+  ...curso,
+  descricao: "bla bla bla",
+  ativo: true,
+};
+
+//spread sem adcionar campos para mais: clona o objeto original, mas cria um novo objeto curso != curso3
+
+let curso3 = {
+  ...curso,
+};
+
+*/
+
+//4. TEMPLATE LITERALS
+
+let a = 2, b = 3;
+let soma = a + b;
+
+console.log(a + " + " + b + " = " + soma);
+
+// escreve dentro de 2 crases
+//faz a soma de fomrma mais concisa e entendível
+//não precisa ter substituição de variáveis, pode ter só string dentro da crase
+console.log(`${a} + ${b} = ${soma}`);
