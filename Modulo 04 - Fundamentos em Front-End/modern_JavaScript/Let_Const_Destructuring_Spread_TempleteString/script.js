e03744d36d6abc95a40cf2c92028755c539b5586
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-11 18:21:43 +0100

    spread

--- Modulo 04 - Fundamentos em Front-End/modern_JavaScript/Let_Const_Destructuring_Spread_TempleteString/script.js
+++ Modulo 04 - Fundamentos em Front-End/modern_JavaScript/Let_Const_Destructuring_Spread_TempleteString/script.js
@@ -18,7 +18,7 @@ function teste() {
   }
   console.log("i: " + i);
 }
-teste(); */
+teste(); 
 
 //2. Usar atribuição via desestruturação
 
@@ -44,3 +44,17 @@ let { nome : nomeCurso, turma, ...outrosCampos } = curso;// ...(rest operator)
 function imprime({nome}) {
    console.log(nome);
 }
+
+*/
+
+//3. Spread Operator
+
+let primos = [2, 3, 5, 7, 11, 13];
+let curso = {
+   nome: "Bootcamp Front End",
+   modulos: 4,
+   presencial: false,
+   turma: 1,
+};
+
+let primos2 = [... primos, 17];
