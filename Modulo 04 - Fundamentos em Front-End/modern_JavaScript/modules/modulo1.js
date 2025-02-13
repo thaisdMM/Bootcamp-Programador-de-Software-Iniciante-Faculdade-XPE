1202772754e3ad6e7dfc14240fbdb63ca3f10485
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-13 16:47:31 +0100

    update export

--- Modulo 04 - Fundamentos em Front-End/modern_JavaScript/modules/modulo1.js
+++ Modulo 04 - Fundamentos em Front-End/modern_JavaScript/modules/modulo1.js
@@ -1,9 +1,7 @@
-function soma(a, b) {
+export function soma(a, b) {
   return a + b;
 }
 
-function subtrai(a, b) {
+export function subtrai(a, b) {
    return a - b;
 }
-
-export { soma, subtrai };
