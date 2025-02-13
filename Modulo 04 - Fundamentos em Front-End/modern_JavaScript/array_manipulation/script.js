436c3ac5efa4aaff37398e6b7fc612991f2fe3a3
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-13 14:31:22 +0100

    for ... of

---
+++ Modulo 04 - Fundamentos em Front-End/modern_JavaScript/array_manipulation/script.js
@@ -0,0 +1,12 @@
+// 1. FOR ... OF
+
+/* Jeito de declarar antes 
+
+for (let i = 0; i < usPresidents.length; i++) {
+  usPresidents[i];
+}
+  */
+
+for (let item of usPresidents) {
+   console.log(item.president)
+}
